cd5d62a99b859ddae255a042038810f24dcba3fe
reynvee29 <reynV1029@gmail.com>
2018-03-14 17:14:15 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
 
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBoB0kaTpYGsF7DQLn9H1KwQs6DnUOb1OQ",
+        authDomain: "angular-social-86168.firebaseapp.com",
+        databaseURL: "https://angular-social-86168.firebaseio.com",
+        projectId: "angular-social-86168",
+        storageBucket: "angular-social-86168.appspot.com",
+        messagingSenderId: "423869547906"
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyBoB0kaTpYGsF7DQLn9H1KwQs6DnUOb1OQ",
+        authDomain: "angular-social-86168.firebaseapp.com",
+        databaseURL: "https://angular-social-86168.firebaseio.com",
+        projectId: "angular-social-86168",
+        storageBucket: "angular-social-86168.appspot.com",
+        messagingSenderId: "423869547906"
     }
 };
